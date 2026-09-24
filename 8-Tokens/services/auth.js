0968afb184262f0657e15bac09445a25b43c7fd6
pavanpai23 <pavanpai2006@gmail.com>
2026-09-24 23:07:59 +0530

    jwt-tokens updated

--- 8-Tokens/services/auth.js
+++ 8-Tokens/services/auth.js
@@ -1,11 +1,23 @@
 const jwt = require("jsonwebtoken");
 const secret = "Pavan123";
 function setUser(user) {
-  return jwt.payload(user, secret);
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret,
+  );
 }
 
 function getUser(token) {
-  return jwt.verify(token, secret);
+  if (!token) return null;
+
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
 }
 
 module.exports = {
